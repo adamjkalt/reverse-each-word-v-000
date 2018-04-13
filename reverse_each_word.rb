2c24127e7b40993bd6758b13ca5c860a6524a5e4
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-13 03:31:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,9 @@ require "pry"
 
 def reverse_each_word(sentence1)
 new_sentence = []
-  sentence1.split.each do |sentence|
-    new_sentence << sentence.reverse
+  sentence1.split.collect do |sentence|
+    sentence.reverse
   end
-  new_sentence.join(" ")
 end
+
+
